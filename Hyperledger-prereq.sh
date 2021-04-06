90b238c36870c6f81afac913263ddbc38acd18ec
Sjoerd <kerkhof.sjoerd@gmail.com>
2021-04-06 12:32:41 +0200

    Add files via upload
    Hyperledger Fabric prerequisite script

---
+++ Hyperledger-prereq.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+yes | sudo apt update
+yes | sudo apt upgrade
+yes | sudo apt install git
+yes | sudo apt install curl
+yes | sudo apt install docker docker-compose
+sudo systemctl start docker
+sudo systemctl enable docker
+u="$USER"
+sudo usermod -a -G docker $u
+sudo curl -sSL https://bit.ly/2ysbOFE | bash -s -- 1.4.0 1.4.0 -d -s
+export PATH=$PWD/bin:$PATH
+git clone https://github.com/anilhelvaci/fabric-2.3-example.git
+chmod 755 ./fabric-2.3-example/
+
+
