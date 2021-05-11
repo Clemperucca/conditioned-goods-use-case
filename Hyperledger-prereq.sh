18c47e9c01a6fa4c3797a932ec2eace2d3cf41ac
SjoerdVanDeKerkhof <kerkhof.sjoerd@gmail.com>
2021-05-11 10:32:52 +0200

    Removed example repo

--- Hyperledger-prereq.sh
+++ Hyperledger-prereq.sh
@@ -11,7 +11,6 @@ u="$USER"
 sudo usermod -a -G docker $u
 sudo curl -sSL https://bit.ly/2ysbOFE | bash -s -- -d -s
 export PATH=$PWD/bin:$PATH
-git clone https://github.com/anilhelvaci/fabric-2.3-example.git
-chmod 755 ./fabric-2.3-example/
+
 
 
